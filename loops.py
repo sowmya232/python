884141fe72637eb2117421a3178c38f2c8186b52
Vanga Sowmya <144512794+sowmya232@users.noreply.github.com>
2025-03-27 06:40:08 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,63 @@
+# for i in range(1, 21):
+#     print(i)
+
+# print('=============================================')
+
+# for i in range(1, 41):
+#     if i % 2 == 0:
+#         print(i)
+
+# print('=====================================')
+
+
+# # skip value in range
+
+# for i in range(2,41, 2):
+#     print(i)
+
+
+# print('===================================================')
+
+# sum = 0
+
+# for i in range(1, 21):
+#     sum += i
+
+# print(sum)
+
+
+# print('---------------------------------------')
+
+# list1 = [ 1, 2, 3, 4, 5]
+
+# # basically manaki values kavali iterable lo ante e for loop rayali
+# for i in  list1:
+#     print(i)
+
+# # basically manaki indexes kavali so we can give conditions based on indexes
+
+# for i in range(0, len(list1)):
+#     print(i)
+#     print(list1[i])
+
+
+# num = 5
+
+# for i in range(1, 21):
+#     print(5 * i)
+
+
+
+# # while loop is based on condition
+
+# num1 = 1
+
+# while(num1 < 21):
+#     print(num1)
+#     num1 += 1
+
+# num1 = 2 
+
+# while(num1  < 41):
+#     print(num1)
+#     num1 += 2
